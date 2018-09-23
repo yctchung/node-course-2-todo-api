a2deda3e41f74f08bf7b5fa62dee6cd16b6a7bd3
unknown <yctchung@gmail.com>
2018-09-23 13:30:39 -0400

    Add Get /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -27,6 +28,37 @@ app.get ('/todos', (req, res) => {
     }, (err) => {
         res.status(400).send(err);
     });
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    
+    /*
+    Validate id using isValid
+        404 - send back empty send
+
+    findById
+        success
+            if todo - send it back
+            if no todo - send back 404 with empty body
+        error
+            400 - send empty body back
+    */
+
+    if (!ObjectID.isValid(id)) {
+        // console.log('Id not valid');
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            // console.log('Id not found');
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((err) => {
+        res.status(400).send();
+    });    
 });
 
 app.listen(3000, () => {
